babf2288798fb318dec4e36c7c889ff8cf7bc437
Matthew Bowler <mbowler03@gmail.com>
2020-05-20 19:37:57 -0400

    added api and html routes, database, handlebars for adding

--- public/js/index.js
+++ public/js/index.js
@@ -95,5 +95,5 @@ var handleDeleteBtnClick = function() {
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+//$submitBtn.on("click", handleFormSubmit);
+//$exampleList.on("click", ".delete", handleDeleteBtnClick);
